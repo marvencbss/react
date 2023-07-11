a85ef36a0a836de98f14958132fce4e955fffb88
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-07-11 20:08:22 -0300

    .

---
+++ app.jsx
@@ -0,0 +1,20 @@
+export default App
+
+function App() {
+
+  return (
+    <>
+    <h2>Heheheheh!!</h2>
+    <p>Eu sou um componente!</p>
+    </>
+  )
+}
+
+export function Introducao() {
+  return (
+    <>
+    <h2>Introdução</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit consequatur quod doloribus. Vel suscipit hic nostrum nemo aut error. Dolorum at voluptas provident! Beatae perferendis voluptates delectus officiis, soluta inventore!</p>
+    </>
+  )
+}
